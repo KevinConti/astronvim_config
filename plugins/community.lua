2f888a4eaf1ef43bcb16261b58561be80271f2f9
KevinConti <kevcon80@gmail.com>
2024-03-24 13:49:52 -0400

    Initial Commit

--- plugins/community.lua
+++ plugins/community.lua
@@ -6,4 +6,43 @@ return {
 
   -- { import = "astrocommunity.colorscheme.catppuccin" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  -- COLORSCHEMES
+  { import = "astrocommunity.colorscheme.tokyonight-nvim" },
+  -- COMPLETION
+  --  Codeium
+  --    Commands:
+  --    <C-g> : Accept suggestion
+  --    <C-;> : Next suggestion
+  --    <C-,> : Previous suggestion
+  --    <C-x> : Clear suggestions
+  --    <leader>; : Toggle Codeium
+  { import = "astrocommunity.completion.codeium-vim" },
+  -- DIAGNOSTICS
+  --  Trouble
+  { import = "astrocommunity.diagnostics.trouble-nvim" },
+  -- EDITING SUPPORT
+  { import = "astrocommunity.editing-support.multicursors-nvim" },
+  { import = "astrocommunity.editing-support.nvim-devdocs" },
+  { import = "astrocommunity.editing-support.rainbow-delimiters-nvim" },
+  { import = "astrocommunity.editing-support.vim-move" },
+  -- LSP
+  { import = "astrocommunity.lsp.lsp-lens-nvim" },
+  -- MOTION
+  { import = "astrocommunity.motion.leap-nvim" },
+  { import = "astrocommunity.motion.flit-nvim" },
+  { import = "astrocommunity.motion.tabout-nvim" },
+  { import = "astrocommunity.motion.harpoon" },
+  { import = "astrocommunity.motion.nvim-surround" },
+  -- PACKS
+  { import = "astrocommunity.pack.cs" },
+  { import = "astrocommunity.pack.docker" },
+  -- { import = "astrocommunity.pack.gleam" }, -- Seems to be out of commission
+  { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.pack.markdown" },
+  { import = "astrocommunity.pack.svelte" },
+  { import = "astrocommunity.pack.tailwindcss" },
+  { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.pack.yaml" },
+  -- SCROLLING
+  { import = "astrocommunity.scrolling.neoscroll-nvim" },
 }
--- plugins/user.lua
+++ plugins/user.lua
@@ -9,4 +9,21 @@ return {
   --     require("lsp_signature").setup()
   --   end,
   -- },
+  --
+  -- COMMON Events
+  -- User AstroFile - Triggered after opening a file
+  -- VeryLazy       - Triggered after starting NeoVim
+  -- BufEnter *.lua - Triggered after opening a Lua file (for filetype-specific extensions)
+  -- InsertEnter    - Triggered after entering insert mode
+  -- LspAttach      - Triggered after starting LSPs.
+  {
+    "folke/todo-comments.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    opts = {},
+    event = "User AstroFile",
+    cmd = { "TodoQuickFix" },
+    keys = {
+      { "<leader>T", "<cmd>TodoTelescope<cr>", desc = "Open TODOs in Telescope" },
+    },
+  },
 }
